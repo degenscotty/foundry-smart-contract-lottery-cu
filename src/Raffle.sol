30ceeb9d2701bf8738954938763d6f532baa2004
degenscotty <scottvermast@gmail.com>
2025-02-04 10:31:26 +0100

    add: Raffle event

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -1,5 +1,59 @@
+// Layout of Contract:
+// version
+// imports
+// errors
+// interfaces, libraries, contracts
+// Type declarations
+// State variables
+// Events
+// Modifiers
+// Functions
+
+// Layout of Functions:
+// constructor
+// receive function (if exists)
+// fallback function (if exists)
+// external
+// public
+// internal
+// private
+// view & pure functions
+
 // SPDX-License-Identifier: MIT
 
 pragma solidity ^0.8.19;
 
-contract Raffle {}
+/**
+ * @title A sample Raffle contract.
+ * @author degenScotty - https://github.com/degenscotty/foundry-smart-contract-lottery-cu
+ * @notice This contract is for creating a sample raffle.
+ * @dev Implements Chainlink VRF 2.5.
+ */
+contract Raffle {
+    /* Errors */
+    error Raffle__SendMoreToRaffle();
+
+    /* State variables */
+    uint256 private immutable i_entranceFee;
+    address payable[] private s_players;
+
+    /* Events */
+    event EnteredRaffle(address indexed player);
+
+    constructor(uint256 entranceFee) {
+        i_entranceFee = entranceFee;
+    }
+
+    function enterRaffle() public payable {
+        if (msg.value < i_entranceFee) revert Raffle__SendMoreToRaffle();
+        s_players.push(payable(msg.sender));
+        emit EnteredRaffle(msg.sender);
+    }
+
+    function pickWinner() public {}
+
+    /* Getter Functions */
+    function getEntranceFee() external view returns (uint256) {
+        return i_entranceFee;
+    }
+}
